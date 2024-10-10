d6c84291ea3ba759aa508788c44a7f60292f7a24
numan012 <numaniqbal560381@gmail.com>
2024-10-10 18:25:52 +0500

    write html in js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Hello world")
+// =================create own server=============
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  console.log(req)
+  //exit your server but we never use this bcz it stop our server
+  // process.exit()
+
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>Node Title Page</title></head>')
+  res.write('<body><h1>This is Server NodeJs Server Text File</h1></body>')
+  res.write('</html>')
+  res.end()
+})
+
+server.listen(3001)
